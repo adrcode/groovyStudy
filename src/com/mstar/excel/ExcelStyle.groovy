06b5aeb5ee6b3d701dc9871e0ff480e92aa4a757
asun <adrian.sun@morningstar.com>
2015-12-04 20:12:48 +1200

    Read Excel File

---
+++ src/com/mstar/excel/ExcelStyle.groovy
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mstar.excel
+
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.hssf.usermodel.HSSFSheet
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.hssf.usermodel.HSSFCellStyle
+import org.apache.poi.hssf.usermodel.HSSFFont
+import org.apache.poi.hssf.usermodel.HSSFRichTextString
+
+import org.apache.poi.hssf.util.HSSFColor
+import org.apache.poi.hssf.util.Region
+
+
+/**
+ *
+ * @author asun
+ */
+class ExcelStyle {
+       static void main(args) {
+            FileOutputStream fileOut = new FileOutputStream("failedCases.xls")
+            HSSFWorkbook wb = new HSSFWorkbook()
+            HSSFSheet sheet = wb.createSheet("Sheet1")
+            HSSFRow row = sheet.createRow(0) 
+            HSSFCellStyle cellStyle = wb.createCellStyle()
+            HSSFFont font = wb.createFont()
+            HSSFFont font2 = wb.createFont()
+        
+            //#########################1. Set the column width ####################
+            sheet.setColumnWidth(0, 3000)      //Column: 0,  Width: 6000
+        
+            //#########################2. Set the foreground color #################### 
+            cellStyle.setFillForegroundColor(HSSFColor.LIGHT_YELLOW.index)
+ //           cellStyle.setFillForegroundColor((short) 13)
+            cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND)
+            
+            //########################3. Set the Border ##############################
+            cellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN)
+            cellStyle.setBottomBorderColor(HSSFColor.RED.index)
+            
+            cellStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN)
+            cellStyle.setLeftBorderColor(HSSFColor.RED.index)
+            
+            cellStyle.setBorderRight(HSSFCellStyle.BORDER_THIN)
+            cellStyle.setRightBorderColor(HSSFColor.RED.index)
+            
+            cellStyle.setBorderTop(HSSFCellStyle.BORDER_THIN)
+            cellStyle.setTopBorderColor(HSSFColor.RED.index)
+            
+            //######################4. Set to Alignment  and set vertical Alignment#########
+            cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER)
+            cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER)
+
+            //#######################5. Set the Font###########################
+            font.setFontName("黑体")
+            font.setFontHeightInPoints((short) 15)    //set the font size
+            
+            font2.setFontName("仿宋_GB2312")
+            font2.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD)
+            font2.setFontHeightInPoints((short) 12)
+            cellStyle.setFont(font2)
+        
+            //#########################6. 自动换行 ####################
+            cellStyle.setWrapText(true)
+            
+            //#########################7. 合并单元格 ####################
+            Region region = new Region(0, (short) 0, 0, (short) 6)
+            sheet.addMergedRegion(region)
+            
+            //#########################8. 设置行高 ####################
+            row.setHeight((short) 400)
+        
+            
+            HSSFCell cell = row.createCell(0)
+            cell.setCellStyle(cellStyle)
+            cell.setCellValue("Hello world!")
+            
+            
+            HSSFRow row2 = sheet.createRow(1)
+            HSSFCell cell2 = row2.createCell(0)
+            
+            //#########################9. HSSFRichTextString ####################
+            HSSFFont ftRed = wb.createFont()
+            ftRed.setColor(HSSFColor.RED.index)
+            ftRed.setStrikeout(true)
+            HSSFFont ftBlue = wb.createFont()
+            ftBlue.setColor(HSSFColor.BLUE.index)            
+            
+            HSSFRichTextString textString = new HSSFRichTextString("first,second")
+            textString.applyFont(0, 4, ftRed)
+            textString.applyFont(6, 11, ftBlue)
+            cell2.setCellValue(textString)
+    
+            wb.write(fileOut)
+            
+       }
+}
+
--- src/com/mstar/excel/NewWorkBook.groovy
+++ src/com/mstar/excel/NewWorkBook.groovy
@@ -7,8 +7,17 @@
 package com.mstar.excel
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.hssf.usermodel.HSSFSheet
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.hssf.usermodel.HSSFCellStyle
+
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFSheet
+import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFCell
+
+
 /**
  *
  * @author asun
@@ -16,30 +25,26 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
 class NewWorkBook {
     static void main(args){
         //########################.xls file##################
-//        HSSFWorkbook wb = new HSSFWorkbook()     
-//        //Add a new excel(.xls) file
-//        FileOutputStream fileOut = new FileOutputStream("failedCases.xls")  
-//        //Add a sheet for the excel file
-//        wb.createSheet("new sheet")
-//        
-//        wb.write(fileOut)
-//        fileOut.close()
-//        
-//         //########################.xlsx file##################
-//         XSSFWorkbook wb2 = new XSSFWorkbook()
-//         wb2.createSheet("new sheet")
-//         FileOutputStream fileOut2 = new FileOutputStream("failedCases.xlsx")
-//         
-//        wb2.write(fileOut2)
-//        fileOut2.close()
-
-        def list = new ArrayList()   
-        FileInputStream fileInput = new FileInputStream("source.xls")
-        HSSFWorkbook wb = new HSSFWorkbook(fileInput)  
-        wb.getSheet("Sheet1").getRow(1).each{cell ->
-            list << cell.getCellType()
-        }
-        println list
+        HSSFWorkbook wb = new HSSFWorkbook()     
+        FileOutputStream fileOut = new FileOutputStream("failedCases.xls")  
+       
+        HSSFSheet sheet1 = wb.createSheet("new sheet")      //Add a Sheet for the excel file
+        HSSFRow row1 = sheet1.createRow(0)              //Add a Row
+        HSSFCell cell1 = row1.createCell(0)             //Add a Cell    
+        cell1.setCellValue("Test")      //Set the value
+        
+        wb.write(fileOut)
+        fileOut.close()
+        
+         //########################.xlsx file##################
+         FileOutputStream fileOut2 = new FileOutputStream("failedCases.xlsx")
+         XSSFWorkbook wb2 = new XSSFWorkbook()
+         XSSFSheet sheet2 = wb2.createSheet("new sheet")           //Add a Sheet for the excel file
+         XSSFRow row2 = sheet2.createRow(0)              //Add a Row
+         XSSFCell cell2 = row2.createCell(0)             //Add a Cell 
+         cell2.setCellValue("Test")                     //Set the value
+         
+        wb2.write(fileOut2)
+        fileOut2.close()
     }
 }
-
